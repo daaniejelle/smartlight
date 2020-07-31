8f2eca3648012484022045d7c0091b411bf6c5e9
daaniejelle <63784313+daaniejelle@users.noreply.github.com>
2020-07-31 22:53:40 +0200

    Add superclass Light

---
+++ src/main/java/com/example/restservice/Superclasses/Light.java
@@ -0,0 +1,48 @@
+package com.example.restservice.Superclasses;
+
+public class Light {
+
+    private int id;
+    private String location;
+    private int xPosition;
+    private int yPosition;
+
+    public Light(int id, String location, int xPosition, int yPosition) {
+        this.id = id;
+        this.location = location;
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getxPosition() {
+        return xPosition;
+    }
+
+    public void setxPosition(int xPosition) {
+        this.xPosition = xPosition;
+    }
+
+    public int getyPosition() {
+        return yPosition;
+    }
+
+    public void setyPosition(int yPosition) {
+        this.yPosition = yPosition;
+    }
+}
